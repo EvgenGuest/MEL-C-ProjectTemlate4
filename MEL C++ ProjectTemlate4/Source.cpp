113b5758af3305fedcdfd1c7deb4331669eac355
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2022-02-18 19:57:27 +0500

    Add project files.

---
+++ MEL C++ ProjectTemlate4/Source.cpp
@@ -0,0 +1,50 @@
+//Персональный шаблон проекта С++
+#include <iostream>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;
+
+template <typename T> int searchIndex(T array[], int length, T key, int begin=0) {
+	for (int i = begin; i < length; i++)
+		if (array[i] == key)
+			return i;
+	return - 1;
+}
+
+template <typename T> int searchLastIndex(T array[], int length, T key, int begin = - 1) {
+	if (begin ==-1)
+		begin = length - 1;
+	for (int i = begin; i >= 0; i--)
+		if (array[i] == key)
+			return i;
+	return -1;
+}
+
+void subWord(char array[], int length, char sym);
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+	int n;
+	// Задание "Подслово"
+	cout << "Изначальный массив:\n";
+	char word[8] = { 'f', 'u', 'n', 'c', 't','i','o','n' };
+	for (int i = 0; i < 8; i++)
+		cout << word[i];
+	cout << endl;
+
+	cout << "Подслово:\n";
+	subWord(word, 8, 'a');
+	cout << endl;
+
+	return 0;
+}
+
+void subWord(char array[], int length, char sym) {
+	int symIndex = searchIndex(array, length, sym);
+	if (symIndex != -1)
+		for (int i = symIndex; i < length; i++)
+			cout << array[i];
+	else
+		cout << "ERROR";
+}
